8d68f2b089302a4209bfd8706d76356b52ad3ab8
jeroentvb <36192730+jeroentvb@users.noreply.github.com>
2022-05-03 20:37:48 +0200

    v1.5.0

--- src/constants/change-notes.ts
+++ src/constants/change-notes.ts
@@ -1,7 +1,8 @@
 const CHANGE_NOTES = `
-* Remove 'play immediately' setting
+* Remove 'play immediately' setting in favour of 'add to queue'
    - Default behaviour is navigating to the selected suggestion
    - To just play and not navigate, hold \`ctrl\` (windows/linux) or \`cmd\` (mac) when selecting it
+   - To add to queue instad of playing the item, enable 'add to queue' and hold hold \`ctrl\` (windows/linux) or \`cmd\` (mac)
 `;
 
 export default CHANGE_NOTES;
