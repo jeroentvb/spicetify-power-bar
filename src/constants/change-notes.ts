5872bc18e7d01198f1acd9bc913b3d4a74c4e7f6
jeroentvb <36192730+jeroentvb@users.noreply.github.com>
2024-05-04 12:50:42 +0200

    release: v1.8.0

--- src/constants/change-notes.ts
+++ src/constants/change-notes.ts
@@ -1,5 +1,5 @@
 const CHANGE_NOTES = `
-You can now use a Spotify URI to navigate using power-bar. Paste a URI in the power-bar and press enter to navigate.
+Power-bar now uses translations for the selected language
 `;
 
 export default CHANGE_NOTES;
